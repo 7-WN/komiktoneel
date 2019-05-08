cbf429da9c0d276217f53f50175cefe623272315
Jesse Op de Beeck <42997689+jesse-opdebeeck@users.noreply.github.com>
2019-05-08 11:29:32 +0200

    onnodige verwijderd uit sponsors

--- src/sponsors.php
+++ src/sponsors.php
@@ -8,38 +8,19 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Sponsors</title>
 </head>
 <body>
-<h1>Alle data</h1>
-<h2>Sponsors</h2>
-    <?php if(mysqli_num_rows($sponsorsResult) > 0){
-      while($row = mysqli_fetch_assoc($sponsorsResult)){ ?>
-      <b><?= $row['naam'] ?></b>
-      <ul>
-        <li>Slogan: <?= $row['slogan'] ?></li>
-        <li>URL: <?= $row['url'] ?></li>
-      </ul>
-      <?php }} ?>
-  </ul>
-  <h2>Gebruikers</h2>
-  <ul>
-    <?php ?>
-  </ul>
-  
-  <h2>Stukken</h2>
-  <ul>
-    <?php ?>
-  </ul>
-  <h2>Reservaties</h2>
-  <ul>
-    <?php ?>
-  </ul>
+<h1>Sponsors</h1>
+  <?php if(mysqli_num_rows($sponsorsResult) > 0){
+    while($row = mysqli_fetch_assoc($sponsorsResult)){ ?>
+    <b><?= $row['naam'] ?></b>
+    <ul>
+      <li>Slogan: <?= $row['slogan'] ?></li>
+      <li>URL: <?= $row['url'] ?></li>
+    </ul>
+  <?php }} ?>
 </body>
 </html>
 </html>
