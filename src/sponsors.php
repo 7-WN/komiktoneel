1ca5c3673c667d03a6e8ee06b3c2b9e351bcddfc
Jesse <jesse.opdebeeck@student.scheppersinstituut.be>
2019-05-18 15:07:34 +0200

    sponsors styling + favicon

--- src/sponsors.php
+++ src/sponsors.php
@@ -12,12 +12,12 @@
 <div class="container">
   <div class="row">
   <?php while($row = mysqli_fetch_assoc($sponsorsResult)){ ?>
-    <div class="card col-3 col-lg-3 col-md-6 col-sm-12 sponsor-card mx-3 my-3">
+    <div class="card col-2 col-lg-3 col-md-4 col-sm-12 sponsor-card mx-3 my-3">
       <img src="<?= $row['image_path'] ?>" alt="Afbeelding <?= $row['naam'] ?>" class="card-img-top">
       <div class="card-body">
         <h5 class="card-title"><?= $row['naam'] ?></h5>
         <p class="card-text"><?= $row['slogan'] ?></p>
-        <a href="<?= $row['url'] ?>" class="btn btn-primary">Bezoek website</a>
+        <a href="<?= $row['url'] ?>" class="btn btn-primary btn-sm" target="_blank">Bezoek website</a>
       </div>
     </div>
   <?php } ?>
