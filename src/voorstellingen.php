1fcb8eae7b116965f90166d38b5e9621797f43ba
Jesse <jesse.opdebeeck@student.scheppersinstituut.be>
2019-05-19 14:59:00 +0200

    start styling van voorstellingen pagina

--- src/voorstellingen.php
+++ src/voorstellingen.php
@@ -2,50 +2,45 @@
   
   include 'php/dbconfig.php';
 
-  $statementStukken = "SELECT * from stukken";
+  $statementStukken = "SELECT * from stukken ORDER BY stuk_id DESC";
   $resStukken = mysqli_query($con, $statementStukken);
 
+  /* DAGEN : per stuk in html
+    $statementDagen = "SELECT * FROM stukken INNER JOIN dagen ON stukken.stuk_id = dagen.stuk_id WHERE stukken.stuk_id = " . $row['stuk_id'];
+    $resDagen = mysqli_query($con, $statementDagen);
+    while($row2 = mysqli_fetch_assoc($resDagen)) {
+      <li><?= date("jS F, Y", strtotime($row2["dag"])) ?></li>
+    } 
+  */
 ?>
 
 <?php $title = "Voorstellingen"; include 'components/head.php' ?>
 <?php include 'components/header.php' ?>
 <div class="container">
-<h1>Voorstellingen</h1>
-<?php if(mysqli_num_rows($resStukken) > 0){
-  while($row = mysqli_fetch_assoc($resStukken)){ ?>
-  <h2><?= $row['titel'] ?></h2>
-  <ul>
-    <li>Beschrijving: <?= $row['beschrijving'] ?></li>
-    <li>Regisseur: <?= $row['regisseur'] ?></li>
-    <li>Schrijver: <?= $row['schrijver'] ?></li>
-    <li>Flyer: <img style="width: 350px; height: 200px; border: 2px solid black;" src="<?= $row['flyer_path'] ?>" /></li>
-    <li><b>Dagen: </b>
-      <ul>
-        <?php 
-        $statementDagen = "SELECT * FROM stukken INNER JOIN dagen ON stukken.stuk_id = dagen.stuk_id WHERE stukken.stuk_id = " . $row['stuk_id'];
-        $resDagen = mysqli_query($con, $statementDagen);
-        while($row2 = mysqli_fetch_assoc($resDagen)){ ?>
-          <li><?= date("jS F, Y", strtotime($row2["dag"])) ?></li>
-        <?php } ?>
-      </ul>
-    </li>
-  </ul>
-  <?php }} ?>
+    <?php while($row = mysqli_fetch_assoc($resStukken)){ ?>
+      <section class="row stuk-row">
+        <div class="col-1"></div>
+        <article class="col-4 <?php echo($row['stuk_id']%2 ? 'stuk-tekst links' : 'stuk-flyer links') ?>">
+          <?php if($row['stuk_id']%2) { ?>
+            <h2 class="stuk-titel links"><?= $row['titel'] ?></h2>
+            <p class="stuk-desc links"><?= $row['beschrijving'] ?></p>
+            <button href="javascript:void(0)" class="btn btn-primary btn-sm">Meer lezen</button>
+          <?php } else { ?>
+            <img class="stuk-flyer" src="<?= $row['flyer_path'] ?>" />
+          <?php } ?>
+        </article>
+        <div class="col-2 stuk-midden"></div>
+        <article class="col-4 <?php echo($row['stuk_id']%2 ? 'stuk-tekst rechts' : 'stuk-flyer rechts') ?>">
+        <?php if($row['stuk_id']%2) { ?>
+            <img class="stuk-flyer" src="<?= $row['flyer_path'] ?>" />
+          <?php } else { ?>
+            <h2 class="stuk-titel rechts"><?= $row['titel'] ?></h2>
+            <p class="stuk-desc rechts"><?= $row['beschrijving'] ?></p>
+            <button href="javascript:void(0)" class="btn btn-primary btn-sm">Meer lezen</button>
+          <?php } ?>
+        </article>
+        <div class="col-1"></div>
+      </section>
+    <?php } ?>
 </div>
 <?php include 'components/foot.php' ?>
-
-<!-- 
-
-  werking:
-  Toon alle voorstellingen
-  Als stuk_id even is, genereer eerst flyer (col-4), dan tijdlijn (col-2), dan tekst (col-4)
-  Oneven = andersom
-
-  Middenstuk is steeds een lijn op volledige hoogte
-  Verbindt zich dus met de lijn erboven en eronder
-  Kader met het jaartal in het verticale midden
-
-  Lagere resoluties:
-  Afbeelding (en tekst?) verstopping zich: alleen titel (en knop)
-
--->
