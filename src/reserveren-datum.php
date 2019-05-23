09602a318d8f025541409a55f00399c88c1a1de1
Jarne.Elst <jarne.elst@student.scheppersinstituut.be>
2019-05-23 12:10:56 +0200

    Reservatie

---
+++ src/reserveren-datum.php
@@ -0,0 +1,46 @@
+<?php 
+    include('./components/head.php');
+    include('./components/header.php');
+?>
+
+
+<main class="container">
+    <section class="row">
+        <div class="arrow-dark col-3">
+            <p class="arrow-text-active">
+                Datum
+            </p>
+        </div>
+        <div class="arrow-light col-3">
+            <p class="arrow-text-inactive">
+                Betaling
+            </p>
+        </div>
+        <div class="arrow-light col-3">
+            <p class="arrow-text-inactive">
+                Afronding
+            </p>
+        </div>
+    </section>
+    <p> Denk eraan: eerst besteld is eerst gesteld. Vroegere reservaties worden dichter tegen het podium geplaatst.
+        We kunnen hier geen uitzonderingen in maken.</p>
+
+
+
+    <article>
+        <input type="checkbox" aria-label="Checkbox for following text input">
+
+    </article>
+
+    <article>
+        <!-- Inladen datums en hoe vol deze zitten -->
+    </article>
+
+
+
+</main>
+
+
+<?php
+include('./components/foot.php');
+?>
---
+++ src/reserveren-gegevens.php
@@ -0,0 +1,12 @@
+<?php 
+    include('./components/head.php');
+    include('./components/header.php');
+
+?>
+
+
+
+
+<?php
+include('./components/foot.php');
+?>
