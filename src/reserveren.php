0922585b296bebedb6d2d23942cd155fc8525737
Jarne.Elst <jarne.elst@student.scheppersinstituut.be>
2019-05-24 13:12:21 +0200

    Update reserveren.php

--- src/reserveren.php
+++ src/reserveren.php
@@ -22,57 +22,65 @@
 <?php $title = "Reserveren"; include 'components/head.php' ?>
 <?php include 'components/header.php' ?>
 <div class="container">
-<main class="container">
-    <section class="row">
-        <div class="arrow-dark col-3">
-            <p class="arrow-text-active">
-                Datum
-            </p>
-        </div>
-        <div class="arrow-light col-3">
-            <p class="arrow-text-inactive">
-                Betaling
-            </p>
-        </div>
-        <div class="arrow-light col-3">
-            <p class="arrow-text-inactive">
-                Afronding
-            </p>
-        </div>
-    </section>
-    <p> Denk eraan: eerst besteld is eerst gesteld. Vroegere reservaties worden dichter tegen het podium geplaatst.
-        We kunnen hier geen uitzonderingen in maken.</p>
+    <main class="container">
+        <section class="row">
+            <div class="arrow-dark col-3">
+                <p class="arrow-text-active">
+                    Datum
+                </p>
+            </div>
+            <div class="arrow-light col-3">
+                <p class="arrow-text-inactive">
+                    Gegevens
+                </p>
+            </div>
+            <div class="arrow-light col-3">
+                <p class="arrow-text-inactive">
+                    Betaling
+                </p>
+            </div>
+        </section>
+        <p> Denk eraan: eerst besteld is eerst gesteld. Vroegere reservaties worden dichter tegen het podium geplaatst.
+            We kunnen hier geen uitzonderingen in maken.</p>
 
-    <article>
-        <input type="checkbox" aria-label="Checkbox for following text input">
-    </article>
-
-  <form class="form" action="./reserveren-gegevens.php" method="POST">
-    <?php if($stukResult){ 
-      /* HAAL STUK UIT DATABASE DAT ACTIEF STAAT */
+        <form class="form" action="./reserveren-gegevens.php" method="POST">
+            <?php if($stukResult){ 
+      /*HAAL STUK UIT DATABASE DAT ACTIEF STAAT*/
       while($stuk = mysqli_fetch_assoc($stukResult)){
         /* HAAL ALLE DAGEN VAN DAT STUK UIT DB */
         $dagenStatement = "SELECT dag_id, dag FROM dagen INNER JOIN stukken ON dagen.stuk_id = stukken.stuk_id WHERE stukken.stuk_id =" . $stuk['stuk_id'];
         $dagenResult = mysqli_query($con, $dagenStatement); ?>
-      <h2>Uw reservatie voor <?= $stuk['titel'] ?></h2>
-      <p>Dagen:</p>
-      <ul>
-      <!-- PER DAG -->
-        <?php while($dag = mysqli_fetch_assoc($dagenResult)){ ?>
-          <li>
-            <span class="col-4"><?= date("jS F, G:i", strtotime($dag["dag"])) ?>u</span>
-            <div class=" col-4 progress">
-              <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valumax="100"></div>
-            </div>
-          </li>
-        <?php } ?>
-    </ul>
-      <h3>Aantal:</h3>
-      <input class="col-1 form-control" type="number" name="aantal" value="<?= isset($aantalKeuze) ? $aantalKeuze : 0 ?>" />
-      <button type="submit" class="button">Volgende</button>
-    <?php }} else /* if($stukResult) */ { ?>
-      Reservaties zijn momenteel niet open
-    <?php } ?>
-  </form>
+            <h2>Uw reservatie voor <?= $stuk['titel'] ?></h2>
+            <p>Dagen:</p>
+
+            <!-- Jarne zijn rommeltje om de dagen in een checkbox form te krijgen -->
+            <ul>
+                <?php while($dag = mysql_fetch_assoc($dagenResult)) {?>
+                <li>
+                    <input type="checkbox" name="dagen" value="<?= $dag['dag_id']?>" </li> <ul>
+
+                    }?>
+                    <!-- PER DAG -->
+                    <?php while($dag = mysqli_fetch_assoc($dagenResult)){ ?>
+                <li>
+
+
+
+                    <span class="col-4"><?= date("jS F, G:i", strtotime($dag["dag"])) ?>u</span>
+                    <div class=" col-4 progress">
+                        <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25"
+                            aria-valuemin="0" aria-valumax="100"></div>
+                    </div>
+                </li>
+                <?php } ?>
+            </ul>
+            <h3>Aantal:</h3>
+            <input class="col-1 form-control" type="number" name="aantal"
+                value="<?= isset($aantalKeuze) ? $aantalKeuze : 0 ?>" />
+            <button type="submit" class="button">Volgende</button>
+            <?php }} else /* if($stukResult) */ { ?>
+            Reservaties zijn momenteel niet open
+            <?php } ?>
+        </form>
 </div>
 <?php include 'components/foot.php' ?>
